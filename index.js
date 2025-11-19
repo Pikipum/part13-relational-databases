9acc0a54a54fa22f05df1fb6b7c6c2b3d4c9d880
Markus Tervonen <markus.tervonen@hotmail.com>
2025-11-19 14:53:00 +0200

    teht 13.4

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+require("dotenv").config();
+const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blogs",
   }
-}
+);
 
-main()
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    console.log(req.body);
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    await Blog.destroy({ where: { id: req.params.id } });
+    console.log("delete succesful");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
